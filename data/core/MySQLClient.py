1c1025d07de738302240a59ce5cc1611616a1207
bats <dev@bats.li>
2024-03-31 17:49:19 +0200

    improved code structure

--- data/core/MySQLClient.py
+++ data/core/MySQLClient.py
@@ -11,6 +11,8 @@ class Client:
             ssl_disabled=True
         )
 
+        self.machine_id = -1
+
         if not self.client.is_connected():
            print("error")
            exit(1)
@@ -25,20 +27,8 @@ class Client:
       result = cursor.fetchall()
       
       if not len(result) == 1:
-          #print("Creating table 'hosts'")
           cursor.execute("CREATE TABLE `database`.`hosts` (`ID` INT NOT NULL AUTO_INCREMENT , `HOST_NAME` VARCHAR(255) NULL , `CPU_MAX` DOUBLE NULL , `MEMORY_MAX` BIGINT NULL , `TX_MAX` BIGINT NULL , `RX_MAX` BIGINT NULL , `CPU_NAME` VARCHAR(255) NULL , `OS_NAME` VARCHAR(255) NULL , `UPTIME` BIGINT NULL , `STORAGE_MAX` INT NULL , `CPU_CORES` INT NULL , `STORAGE_USED` INT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;")
           self.client.commit()
-
-      machine_name = platform.node()
-      cursor.execute(f"SELECT ID FROM hosts WHERE HOST_NAME = '{machine_name}'") 
-      result = cursor.fetchall()
-      
-      if len(result) == 0:
-          cursor.execute(f"INSERT INTO hosts (HOST_NAME) VALUES ('{machine_name}')")
-          cursor.execute(f"SELECT ID FROM hosts WHERE HOST_NAME = '{machine_name}'") 
-          result = cursor.fetchall()
-
-      self.machine_id = result[0][0]
 
       # -------------------------------- METRICS TABLE
       cursor.execute("SHOW TABLES LIKE 'metrics'")
@@ -53,7 +43,42 @@ class Client:
         cursor.execute("DELETE FROM metrics WHERE TIMESTAMP < NOW() - INTERVAL 15 MINUTE;")
         self.client.commit()
 
-    def track(self, name, cpu, mem, tx, rx):
+    def track_container(self, name, cpu, mem, tx, rx):
+        if self.machine_id == -1:
+            print("ERROR: Instance has not been identified. Exiting")
+            exit(3)
+
         cursor = self.client.cursor()
         cursor.execute(f"INSERT INTO metrics (CONTAINER, CPU, MEMORY, TX, RX, HOST_ID) VALUES ('{name}', {cpu}, {mem}, {tx}, {rx}, {self.machine_id})")
         self.client.commit()
+
+    def track_storage(self, used_storage):
+        if self.machine_id == -1:
+            print("ERROR: Instance has not been identified. Exiting")
+            exit(4)
+
+        cursor = self.client.cursor()
+        cursor.execute(f"UPDATE hosts SET STORAGE_USED = {used_storage} WHERE ID = {self.machine_id}")
+        self.client.commit()
+
+    def identify(self, host_name):
+        cursor = self.client.cursor()
+        # TODO hier machine_name modularisieren
+        cursor.execute(f"SELECT ID FROM hosts WHERE HOST_NAME = '{host_name}'") 
+        result = cursor.fetchall()
+        try:
+            self.machine_id = result[0][0]
+        except:
+            print(f"ERROR: This host '{host_name}' has not been setup in the 'host' Table! Please setup first.")
+            exit(2)
+
+    def setup_host(self, host_name, cpu_max, memory_max, storage_max, tx_max, rx_max, cpu_name, os_name, uptime, cpu_cores):
+        cursor = self.client.cursor()
+        cursor.execute(f"SELECT ID FROM hosts WHERE HOST_NAME = '{host_name}'") 
+        result = cursor.fetchall()
+
+        if len(result) == 0:
+            cursor.execute(f"INSERT INTO hosts (HOST_NAME, CPU_MAX, MEMORY_MAX, STORAGE_MAX, TX_MAX, RX_MAX, CPU_NAME, OS_NAME, UPTIME, CPU_CORES) VALUES ('{host_name}', {cpu_max}, {memory_max}, {storage_max}, {tx_max}, {rx_max}, '{cpu_name}', '{os_name}', {uptime}, {cpu_cores})")
+        else:
+            cursor.execute(f"UPDATE hosts SET CPU_MAX = {cpu_max}, MEMORY_MAX = {memory_max}, STORAGE_MAX = {storage_max}, TX_MAX = {tx_max}, RX_MAX = {rx_max}, CPU_NAME = '{cpu_name}', OS_NAME = '{os_name}', UPTIME = {uptime}, CPU_CORES = {cpu_cores} WHERE HOST_NAME = '{host_name}'")
+
--- data/core/stats.py
+++ data/core/stats.py
@@ -1,16 +1,42 @@
 import docker
 import time
 import json
-from MySQLClient import Client
 from _thread import *
+import platform
+import os
+
+from MySQLClient import Client
+
+# ------------------------------------------------------------------------------------------
 
 # docker API
 client = docker.from_env()
 
-
-
 # set of currently running container names
 container_list = set()
+
+# host_name, used as identifier of this cloudwatch instance
+host_name = os.environ.get('CW_SERVER_NAME')
+
+# ------------------------------------------------------------------------------------------
+
+def main():
+    db = Client("db", "user", "test", "database")
+
+    db.setup_host(host_name, 600, 32, 1000, 7, 7, "M1", "OSX", 500, 6)
+
+    db.identify(host_name)
+
+    # This 'parent' Thread continuously searches for new containers and tracks them
+    # in each their own new thread simultaneously
+    start_new_thread(find_containers, ())
+
+    while True:
+        db.cleanup()
+        db.track_storage(300)
+        time.sleep(3)
+
+# ------------------------------------------------------------------------------------------
 
 # this is taken directly from docker client:
 # https://github.com/docker/docker/blob/28a7577a029780e4533faf3d057ec9f6c7a10948/api/client/stats.go#L309
@@ -27,6 +53,7 @@ def calculate_cpu_percent(status):
 
 def collect_stats(container_name):
     db = Client("db", "user", "test", "database")
+    db.identify(host_name)
     container = client.containers.get(container_name)
     for stats in container.stats(decode=None, stream=True):
         #print(f"Collecting stats for {container_name}")
@@ -38,7 +65,7 @@ def collect_stats(container_name):
         tx_b = stats["networks"]["eth0"]["tx_bytes"]
         rx_b = stats["networks"]["eth0"]["rx_bytes"]
 
-        db.track(container_name, cpu, memory_b, tx_b, rx_b)
+        db.track_container(container_name, cpu, memory_b, tx_b, rx_b)
 
     print(f"Container {container_name} has been stopped.")
     container_list.remove(container_name)
@@ -56,13 +83,6 @@ def find_containers():
 
         time.sleep(5)
 
-
-start_new_thread(find_containers, ())
-
-while True:
-    db = Client("db", "user", "test", "database")
-    db.cleanup()
-    time.sleep(3)
-
-# todo loop checking for new containers
-# todo thread exiting, remove container_name from set
+# ----------------- main function -------------------
+if __name__ == "__main__":
+    main()
